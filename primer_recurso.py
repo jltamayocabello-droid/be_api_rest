2ccdc71068db648d3e78aef6d07a25d43dfaedb8
Jorge <jltamayocabello@gmail.com>
2026-08-26 18:44:49 -0400

    Ejemplo de versionado de APIs

--- primer_recurso.py
+++ primer_recurso.py
@@ -20,3 +20,17 @@ def create_user():
     return {
         "message": "User created"
             }
+
+# Versionado de APIs (URI y Header versioning)
+
+@app.get("/api/v1/users")
+def get_users_v1():
+    return [
+        {"id": 1, "name": "Guillermo Beltran"},
+    ]
+
+@app.get("/api/v2/users")
+def get_users_v2():
+    return [
+        {"id": 1, "full_name": "Guillermo Beltran Rios"},
+    ]
