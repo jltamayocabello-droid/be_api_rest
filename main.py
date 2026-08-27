d50b752b0ecf0ba82e086f6c0e0cf79beb4e41c2
Jorge <jltamayocabello@gmail.com>
2026-08-27 13:11:10 -0400

    Se revisa ejemplo de JSON, estructura y tipos de datos

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/user")
+def get_user():
+    return {
+        "id": 1, 
+        "name": "Guillermo Beltran",
+        "active": True
+    }
