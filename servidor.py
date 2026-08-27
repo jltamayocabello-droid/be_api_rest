3b6bbf9f1fcb6f1dd7d8d271bdd71828b7d85bc3
Jorge <jltamayocabello@gmail.com>
2026-08-27 13:46:27 -0400

    Se agrega ejemplo de manejo de fechas y horas

--- servidor.py
+++ servidor.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from datetime import datetime
+from datetime import timezone
 
 app = FastAPI()
 
+# Endcoding UTF8
 @app.get("/text")
 def get_text():
     return {"message": "Joaquin Vidal : APIs y programación - año"
     }
+
+# Manejo de fechas y horas en APIs
+
+@app.get("/time")
+def get_time():
+    now = datetime.now(timezone.utc)
+    iso_date = now.isoformat()
+
+    return {"time": iso_date}
