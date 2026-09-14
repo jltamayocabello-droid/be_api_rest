c3d0eb0257ecc4fb00da060d4b72112462731c87
Jorge <jltamayocabello@gmail.com>
2026-09-13 21:29:24 -0300

    Se crea test users

---
+++ test_users.py
@@ -0,0 +1,10 @@
+import requests
+
+base_url = "http://localhost:8000/users"
+
+def test_get_users():
+    response = requests.get(base_url)
+    assert response.status_code == 200
+    data = response.json()
+    assert isinstance(data, list)
+    assert len(data) > 0
